c8140899fc815f78d284c7dd6cebde8a56a05d70
Ruslan Negoda <iarjuna108@gmail.com>
2022-10-29 23:53:30 +0300

    123

--- src/components/Btn_feedback/Btn_feedback.jsx
+++ src/components/Btn_feedback/Btn_feedback.jsx
@@ -1,17 +1,36 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-export const BtnFeedback = ({ feedback }) => {
-  return (
-    <>
-      <button>{feedback}</button>
-      <h2>Statistics</h2>
-      <p>
-        Good: <span>0</span>
-      </p>
-    </>
-  );
-};
+export class BtnFeedback extends Component {
+  // static defaultProps = {
+  //   feedback: '',
+  // };
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-Event.propTypes = {
-  feedback: PropTypes.string.isRequired,
-};
+  numberGood = () => {
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+  };
+  render() {
+    return (
+      <>
+        <button type="button" onClick={this.numberGood}>
+          Good
+        </button>
+        <h2>Statistics</h2>
+        <p>
+          Good: <span>{this.state.good}</span>
+        </p>
+      </>
+    );
+  }
+}
+
+// Event.propTypes = {
+//   feedback: PropTypes.string.isRequired,
+// };
