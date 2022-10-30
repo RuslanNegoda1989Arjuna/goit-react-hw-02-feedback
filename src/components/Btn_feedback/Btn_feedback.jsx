98ce04921dbe9b9d031beeb77da54c74fd52071e
Ruslan Negoda <iarjuna108@gmail.com>
2022-10-30 09:31:34 +0200

    123

--- src/components/Btn_feedback/Btn_feedback.jsx
+++ src/components/Btn_feedback/Btn_feedback.jsx
@@ -1,10 +1,8 @@
 // import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { Button, Container } from './Btn_feedback.styled';
 
 export class BtnFeedback extends Component {
-  // static defaultProps = {
-  //   feedback: '',
-  // };
   state = {
     good: 0,
     neutral: 0,
@@ -12,16 +10,27 @@ export class BtnFeedback extends Component {
   };
 
   numberGood = () => {
+    console.log(this.state);
     this.setState(prevState => {
       return { good: prevState.good + 1 };
     });
   };
+
   render() {
     return (
       <>
-        <button type="button" onClick={this.numberGood}>
-          Good
-        </button>
+        <Container>
+          <Button type="button" onClick={this.numberGood}>
+            Good
+          </Button>
+          <Button type="button" onClick={this.numberNeutral}>
+            Neutral
+          </Button>
+          <Button type="button" onClick={this.numberBad}>
+            Bad
+          </Button>
+        </Container>
+
         <h2>Statistics</h2>
         <p>
           Good: <span>{this.state.good}</span>
---
+++ src/components/Btn_feedback/Btn_feedback.styled.js
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Button = styled.button`
+  padding: 10px;
+  margin: 10px;
+  border: none;
+  border-radius: 3px;
+  box-shadow: rgb(0 0 0 / 16%) 0px 3px 6px, rgb(0 0 0 / 23%) 0px 3px 6px;
+  cursor: pointer;
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover {
+    background-color: rgba(255, 183, 48, 1);
+    scale: 1.2;
+  }
+`;
